3dd7201c0548c33c15d56b1d29ae2ba90b7bc31c
kioniv <s4mu24i@aim.com>
2018-05-10 21:56:24 -0400

    Making send attack use the tamper monkey script if the attack doesn't need to be sent right away.

--- Selenium_Test.py
+++ Selenium_Test.py
@@ -7,28 +7,28 @@ import getpass
 import random
 import copy
 import time
-try:
-    import requests
-except ImportError:
-    import pip
-    pip.main(['install', 'requests'])
-    import requests
+import requests
 
 driver = webdriver.Chrome()
 driver.implicitly_wait(5)
 world = 101
 units = {'spear':0, 'sword':0, 'axe':0, 'archer':0, 'spy':0, 'light':0, 'marcher':0, 'heavy':0, 'ram':0, 'catapult':0, 'knight':0, 'snob':0}
 setArrivalTimeScript = requests.get("https://raw.githubusercontent.com/kioniv/TW_Tamper_Monkey/master/Set_Arrival_Time").text
-username = raw_input("Enter your username: ")
+username = input("Enter your username: ")
 passwd = getpass.getpass("Enter your password: ")
+
+def SafeSendKeys(target, keys):
+    for char in keys:
+        time.sleep(random.uniform(.05, .34))
+        target.send_keys(char)
 
 def Login(username, passwd, world):
     driver.get("https://tribalwars.net")
     loginBtn = driver.find_element_by_class_name("btn-login")
     userNameField = driver.find_element_by_name("username")
     passwordField = driver.find_element_by_name("password")
-    userNameField.send_keys(username)
-    passwordField.send_keys(passwd)
+    SafeSendKeys(userNameField, username)
+    SafeSendKeys(passwordField, passwd)
     loginBtn.click()
     WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "world_button_active")))
     worldBtn = driver.find_elements_by_class_name("world_button_active")
@@ -36,14 +36,16 @@ def Login(username, passwd, world):
         for worldEntry in worldBtn:
             if str(world) in worldEntry.text:
                 worldEntry.click()
+        try:
+            driver.find_element_by_class_name("btn-default").click()
+        except:
+            pass
     except:
         assert driver.find_element_by_id("serverTime")
 
 def SendAttack(sendingVillage, targetVillage, arrivalTime, sendUnits):
     # navigate to correct village
-    if str(sendingVillage).lower() == "current":
-        driver.find_element_by_class_name("village").click()
-    else:
+    if str(sendingVillage).lower() != "current":
         NavCombinedOverview()
         RandWait()
         villageList = driver.find_elements_by_class_name("quickedit-label")
@@ -70,7 +72,36 @@ def SendAttack(sendingVillage, targetVillage, arrivalTime, sendUnits):
     RandWait()
     #
     if arrivalTime != "":
-        driver.execute_async_script(str(setArrivalTimeScript))
+        driver.execute_script(str(setArrivalTimeScript))
+        time.sleep(.5)
+        milliseconds = arrivalTime[-3:]
+        arrivalTime =arrivalTime[:-4]
+        driver.find_element_by_id("delayInput").clear()
+        driver.find_element_by_id("delayInput").send_keys("0")
+        driver.find_element_by_id("delayButton").click()
+        driver.find_element_by_id("reloadInput").clear()
+        driver.find_element_by_id("reloadInput").send_keys("0")
+        driver.find_element_by_id("reloadButton").click()
+        driver.find_element_by_id("arrTime").click()
+
+        WebDriverWait(driver, 3).until(expected_conditions.alert_is_present(),
+                                        'Timed out waiting for PA creation ' +
+                                        'confirmation popup to appear.')
+        alert = driver.switch_to.alert
+        alert.send_keys(arrivalTime)
+        alert.accept()
+
+
+        WebDriverWait(driver, 3).until(expected_conditions.alert_is_present(),
+                                       'Timed out waiting for PA creation ' +
+                                       'confirmation popup to appear.')
+        alert = driver.switch_to.alert
+        alert.send_keys(milliseconds)
+        alert.accept()
+
+        WebDriverWait(driver, 999999999999).until(expected_conditions.url_changes)
+        print ("Sent " + str(sendUnits) + " from " + str(sendingVillage) + " to " + str(targetVillage)
+               + "\nArrival set for " + str(arrivalTime) + ":" + str(milliseconds))
 
     else:
         driver.find_element_by_id("troop_confirm_go").click()
@@ -87,21 +118,16 @@ def EnterUnits(sendUnitsDict):
             except:
                 pass
 
-def SafeSendKeys(target, keys):
-    for char in keys:
-        RandWait()
-        target.send_keys(char)
-
 
 def NavCombinedOverview():
     driver.get("https://en" + str(world) + ".tribalwars.net/game.php?&screen=overview_villages")
 
 def RandWait():
-    time.sleep(random.uniform(.2, .8))
+    time.sleep(random.uniform(.2, .7))
 
 Login(username, passwd, world)
 
-myattack = copy.deepcopy(units)
-myattack['spear'] = 1
+myAttack = copy.deepcopy(units)
+myAttack['spear'] = 1
 
-SendAttack("current", "560|454", "", myattack)
+SendAttack("current", "560|454", "02:43:00:000", myAttack)
